43839ae36adbf4088f5355bd6097f098db1f473d
ovniarte <abrahanjoses54@gmail.com>
2023-08-30 17:52:52 -0600

    Agregar Codigo

---
+++ LABTXGA.py
@@ -0,0 +1,8 @@
+def main():
+    print("¡Bienvenido al juego!")
+    nombre_jugador = input("Por favor, ingresa tu nombre: ")
+    
+    print(f"\nHola {nombre_jugador} ¡Comencemos a jugar!")
+
+if __name__ == "__main__":
+    main()
